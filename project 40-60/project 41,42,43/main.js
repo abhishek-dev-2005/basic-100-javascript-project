2b11b269a7fddd322c7df7f2f289e65cf364bda8
abhishek vishwakarma <abhishekvishwa2005@gmail.com>
2026-08-26 21:14:53 +0530

    loops question

---
+++ project 40-60/project 41,42,43/main.js
@@ -0,0 +1,22 @@
+console.log("print 1 se 10");
+for (i = 1; i <= 10; i++) {
+  console.log("i =", i);
+}
+
+console.log("print 1 se n ");
+let j = Number(prompt("enter a number"));
+for (i = 1; i <= j; i++) {
+  console.log("j =", i);
+}
+
+console.log("print n se 1 ");
+let k = Number(prompt("enter a number"));
+for (i = 10; i >= k; i--) {
+  console.log("k =", i);
+}
+
+// console.log("print 1 se n ");
+// let j = Number(prompt("enter a number"));
+// for (i = 1; i <= j; i++) {
+//   console.log("j =", i);
+// }
