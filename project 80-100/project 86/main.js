e600a28f85296d03ec80e6995e73e8f81cf3db78
abhishek vishwakarma <abhishekvishwa2005@gmail.com>
2026-09-07 11:15:54 +0530

    number triangle

---
+++ project 80-100/project 86/main.js
@@ -0,0 +1,16 @@
+let print = document.querySelector(".container");
+
+print.addEventListener("click", () => {
+  let rows = Number(prompt("Enter stars value:"));
+
+  let rowStr = "";
+
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= i; j++) {
+      rowStr += j;
+    }
+    rowStr += "<br>";
+  }
+
+  print.innerHTML = rowStr;
+});
